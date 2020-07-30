36e3e3cd483556b84b624c4834b22b102fe7294d
wild.chamo <joselitobedoya230@gmail.com>
2020-07-30 15:12:34 -0500

    antesd e pruebas!

---
+++ buzz.js
@@ -0,0 +1,20 @@
+var number;
+
+for(i=0;i<101;i++){
+if(esdivisible(number,3)&&esdivisible(number,5)){
+    document.write("Fizzbuzz <br>" )
+}else if(esdivisible(number,3)){
+    document.write ("Fizz <br>")
+}else if(esdivisible(number,5)){
+    document.write ("Buzz <br>")
+}
+else{
+    document.write(number +"<br>");
+}
+}
+
+
+function esdivisible(number, dividor){
+if
+
+}
